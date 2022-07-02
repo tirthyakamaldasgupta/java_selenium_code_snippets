49457ecac01bea6846327663476703be97d04c65
tirthyakamaldasgupta <70899540+tirthyakamaldasgupta@users.noreply.github.com>
2022-07-02 21:19:04 +0530

    Update AcceptInsecureCertsTest.java

--- AcceptInsecureCertsTest.java
+++ AcceptInsecureCertsTest.java
@@ -1,4 +1,4 @@
-package sharedCapibilities;
+package sharedCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
